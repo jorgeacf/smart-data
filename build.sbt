5b083f46fa3eace92089a667c375efeb6a20ec44
Jorge Figueiredo <jorgefigueiredo@outlook.com>
2017-09-30 22:08:53 +0100

    Added pom.xml

--- build.sbt
+++ build.sbt
@@ -2,5 +2,11 @@ name := "src"
 
 version := "0.1"
 
-scalaVersion := "2.12.3"
-        
+scalaVersion := "2.11.8"
+
+fork := true
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" % "spark-core_2.11" % "2.2.0",// % "provided"
+  "org.scalatest" % "scalatest_2.11" % "3.0.4" % "test"
+)
--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,10 +1,19 @@
-
+import org.apache.spark.{SparkConf, SparkContext}
 
 object MainApp {
 
   def main(args: Array[String]): Unit = {
 
-    println("...")
+    val sparkConf = new SparkConf()
+    sparkConf.setAppName("Datalab application")
+    sparkConf.setMaster("local[*]")
+    //sparkConf.set("spark.ui.showConsoleProgress", "false")
+    sparkConf.set("spark.driver.allowMultipleContexts", "true")
+    val sparkContext = new SparkContext(sparkConf)
+
+    val list = sparkContext.parallelize(List(1, 2, 3, 4, 5),1).map(_ * 2)
+
+    list.foreach(println)
 
   }
 
